3c590b27b7ce4907582746a642e0d892b58b3af6
David Kim <jk2537@cornell.edu>
2022-08-27 04:31:18 -0400

    added body-parser

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const axios = require("axios");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = process.env.PORT || 3000;
